4a0e3e1e0be053762802d08495c39d250c88c587
yuki-mori-0327 <01252000yuuki@gmail.com>
2025-10-22 12:32:50 +0900

    Fix syntax: remove diff markers in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,5 @@
-// app/javascript/application.js
- import "@hotwired/turbo-rails"
- import "controllers"
+import "@hotwired/turbo-rails"
+import "controllers"
 
-- import "./item_price"
-- import "./card"
-+ import "item_price"
-+ import "card"
+import "item_price"
+import "card"
