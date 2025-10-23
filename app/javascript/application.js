c873fcebac435649b0ed7aa424ba8dc42b39d307
yuki-mori-0327 <01252000yuuki@gmail.com>
2025-10-23 11:43:57 +0900

    fix import controllers error and enable price auto-calc

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 import "@hotwired/turbo-rails"
-import "controllers"
 import "item_price"
 import "card"
 
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
-// app/javascript/controllers/index.js
-import { application } from "./application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
-// controllers フォルダ配下の *_controller.js を自動登録
-eagerLoadControllersFrom("controllers", application)
+window.Stimulus = Application.start()
+const context = require.context("controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
