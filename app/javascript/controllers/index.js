d8916de52b8add8d9588c115f88f6691d990af88
yuki-mori-0327 <01252000yuuki@gmail.com>
2025-10-24 14:05:03 +0900

    Add credit_card_controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,4 @@
-// app/javascript/controllers/index.js
-import { application } from "./application"
-import CreditCardController from "./credit_card_controller"
+import { application } from "controllers/application"
+import CreditCardController from "controllers/credit_card_controller"
 
-// これだけ登録すればOK（stimulus-loading等は不要）
 application.register("credit-card", CreditCardController)
