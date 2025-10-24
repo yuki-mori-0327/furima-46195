e1e87e48cf4bcdb6950fc977b02bfb0710ce98f0
yuki-mori-0327 <01252000yuuki@gmail.com>
2025-10-24 09:13:54 +0900

    Add credit card input formatting & validation with Stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,6 @@
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// app/javascript/controllers/index.js
+import { application } from "./application"
+import CreditCardController from "./credit_card_controller"
+
+// これだけ登録すればOK（stimulus-loading等は不要）
+application.register("credit-card", CreditCardController)
