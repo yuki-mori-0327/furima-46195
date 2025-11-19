1b51090fde792abf9cb61bb64643dddc97d5ffe0
yuki-mori-0327 <01252000yuuki@gmail.com>
2025-11-19 18:29:44 +0900

    Add Payjp import and token setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,8 @@ import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 
+import "payjp" 
+
 import "controllers"  
 
 function setupPriceCalc() {
