e0902ba54afe363f30564c26061bef5afe092ed1
yuki-mori-0327 <01252000yuuki@gmail.com>
2026-01-11 20:47:16 +0900

    商品購入機能を実装

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,67 +1,67 @@
-# class OrderForm
-#   include ActiveModel::Model
-#   include ActiveModel::Validations::Callbacks
-#
-#   attr_accessor :user_id, :item_id,
-#                 :postal_code, :prefecture_id, :city, :block, :building,
-#                 :phone_number, :token
-#
-#   before_validation :normalize_fields
-#
-#   with_options presence: true do
-#     validates :user_id
-#     validates :item_id
-#
-#     validates :postal_code,
-#               format: { with: /\A\d{3}-\d{4}\z/,
-#                         message: 'is invalid. Include hyphen(-)' }
-#
-#     validates :city
-#     validates :block
-#
-#     validates :phone_number,
-#               format: { with: /\A\d{10,11}\z/,
-#                         message: 'is invalid' }
-#
-#     validates :token
-#   end
-#
-#   validates :prefecture_id,
-#             numericality: { other_than: 0, message: "can't be blank" }
-#
-#   def save
-#     ActiveRecord::Base.transaction do
-#       order = Order.create!(user_id: user_id, item_id: item_id)
-#
-#       Address.create!(
-#         order_id: order.id,
-#         postal_code: postal_code,
-#         prefecture_id: prefecture_id,
-#         city: city,
-#         block: block,
-#         building: building,
-#         phone_number: phone_number
-#       )
-#     end
-#     true
-#   rescue ActiveRecord::RecordInvalid
-#     false
-#   end
-#
-#   private
-#
-#   def normalize_fields
-#     self.postal_code  = to_hankaku(postal_code).to_s
-#     self.phone_number = to_hankaku(phone_number).to_s.gsub(/-/, '')
-#
-#     self.prefecture_id = prefecture_id.to_i if prefecture_id.present?
-#   end
-#
-#   def to_hankaku(str)
-#     return str if str.blank?
-#
-#     s = str.to_s.unicode_normalize(:nfkc)
-#
-#     s.gsub(/[‐-‒–—―−ーｰ]/, '-')
-#   end
-# end
+class OrderForm
+  include ActiveModel::Model
+  include ActiveModel::Validations::Callbacks
+
+  attr_accessor :postal_code, :prefecture_id, :city,
+                :addresses, :building, :phone_number,
+                :user_id, :item_id, :token
+
+  before_validation :normalize_fields
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+
+    validates :postal_code,
+              format: { with: /\A\d{3}-\d{4}\z/,
+                        message: 'is invalid. Include hyphen(-)' }
+
+    validates :city
+    validates :addresses
+
+    validates :phone_number,
+              format: { with: /\A\d{10,11}\z/,
+                        message: 'is invalid' }
+
+    validates :token
+  end
+
+  validates :prefecture_id,
+            numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    ActiveRecord::Base.transaction do
+      order = Order.create!(user_id: user_id, item_id: item_id)
+
+      Address.create!(
+        order_id: order.id,
+        postal_code: postal_code,
+        prefecture_id: prefecture_id,
+        city: city,
+        addresses: addresses,
+        building: building,
+        phone_number: phone_number
+      )
+    end
+    true
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+
+  private
+
+  def normalize_fields
+    self.postal_code  = to_hankaku(postal_code).to_s
+    self.phone_number = to_hankaku(phone_number).to_s.gsub(/-/, '')
+
+    self.prefecture_id = prefecture_id.to_i if prefecture_id.present?
+  end
+
+  def to_hankaku(str)
+    return str if str.blank?
+
+    s = str.to_s.unicode_normalize(:nfkc)
+
+    s.gsub(/[‐-‒–—―−ーｰ]/, '-')
+  end
+end
